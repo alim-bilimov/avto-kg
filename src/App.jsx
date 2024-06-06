0f008cc7a3fffcefe44f2d79dfd31fc031eb52f8
adahan <adahanjolchuev@gmail.com>
2024-06-06 13:37:49 +0600

    heroAdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header";
-import Home from "./pages/home";
+import Home from "./pages/home/index";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
